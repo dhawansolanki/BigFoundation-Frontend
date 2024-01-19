c6ab4e27b8b0a51a1c068e6b559738c093a84720
J-ON-AS <118046576+J-ON-AS@users.noreply.github.com>
2024-01-19 23:34:41 +0530

    Navbar completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,25 +2,31 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import './Header.css'
 export const Header = () => {
-  return (
+  const closeDropdown = () => {
+    const navbarToggler = document.getElementById('navbarToggler');
+    if (navbarToggler) {
+      navbarToggler.click();
+    }
+  };
+  return (    
     <>
-      <div >
+       <div>
         <nav className="navbar navbar-expand-md sticky-top bg-light navbar-light">
           <div className="container-fluid">
-            <Link className="navbar-brand d-md-none " to="/"><img src="/bigf.png"
-              style={{ height: '69px' }}
-              alt="Logo" /></Link><button data-bs-toggle="collapse"
-                className="navbar-toggler  "
-                data-bs-target="#navcol-1">
-              <span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span>
+            <Link className="navbar-brand" to="/">
+              <img src="/bigf.png" style={{ height: '69px' }} alt="Logo" />
+            </Link>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navcol-1"
+            >
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div className='d-flex justify-content-start'>  <Link to='/' className="navbar-brand d-none d-md-block me-0"><img
-              src="/bigf.png"
-              style={{ height: '69px' }}
-              alt="Logo"
-            /></Link></div>
-            <div className="collapse  navbar-collapse " id="navcol-1">
-              <ul className="navbar-nav d-flex justify-content-end mx-auto my-auto w-100">
+            <div className="collapse navbar-collapse" id="navcol-1">
+              <ul className="navbar-nav ms-auto">
 
                 <li style={{ paddingRight: '12px' }} className="nav-item">
                   <Link className="nav-link text-center" to="/">&nbsp;<b>Home</b></Link>
@@ -48,24 +54,24 @@ export const Header = () => {
                 <li style={{ paddingRight: '12px' }} className="nav-item pb-2 pb-md-0">
                   <Link className="nav-link text-nowrap text-center" to="/contact">&nbsp;<b>Contact</b></Link>
                 </li>
-                <li  style={{ paddingRight: '12px' ,backgroundColor:'#5935f7'}} className="nav-item dropdown rounded">
-                  <Link  style={{color:'white'}} className="nav-link " to="/register">&nbsp;<b>Register</b></Link>
-                  <div style={{right:'0'}} className="dropdown-menu position-absolute"  aria-labelledby="navbarDropdown">
+                <li style={{ paddingRight: '12px', backgroundColor: '#5935f7' }} className="nav-item dropdown rounded">
+                  <button style={{ color: 'white' , right: '0', maxWidth: '200px'}} onClick={closeDropdown} className="nav-link ">&nbsp;<b>Register</b></button>
+                  <div style={{ right: '0', maxWidth: '200px' }} className="dropdown-menu position-absolute dropdown-menu-end" aria-labelledby="navbarDropdown">
 
-                  <Link className="dropdown-item "    to="/register">Students</Link>
-                  <Link className="dropdown-item "    to="/register">Alumini</Link>
-                  <Link className="dropdown-item "   to="/register">Faculty</Link>
-                  <Link className="dropdown-item "   to="/register">Company/Industry</Link>
-                  <Link className="dropdown-item "   to="/register">StartUp</Link>
-                </div>
+                    <Link onClick={closeDropdown} className="dropdown-item text-nowrap" to="/register">Students</Link>
+                    <Link onClick={closeDropdown} className="dropdown-item text-nowrap" to="/register">Alumini</Link>
+                    <Link onClick={closeDropdown} className="dropdown-item text-nowrap" to="/register">Faculty</Link>
+                    <Link onClick={closeDropdown} className="dropdown-item text-nowrap" to="/register">Company/Industry</Link>
+                    <Link onClick={closeDropdown} className="dropdown-item text-nowrap" to="/register">StartUp</Link>
+                  </div>
                 </li>
-                
               </ul>
             </div>
           </div>
         </nav>
       </div>
     </>
-  )
-}
+  );
+};
+
 export default Header;
